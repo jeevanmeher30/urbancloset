dec7f11c6e182a8b615a71537419444321b04363
Jeevan Meher <jeevanmeher30@gmail.com>
2026-09-04 10:56:54 +0530

    bug clear

--- src/pages/admin/EditProduct.jsx
+++ src/pages/admin/EditProduct.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const EditProduct = () => {
 
